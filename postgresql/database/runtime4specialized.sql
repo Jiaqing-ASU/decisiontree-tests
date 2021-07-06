a36696d9170b9fd1654748af37128442a25def3c
Jiaqing Chen <jchen501@asu.edu>
2021-07-06 14:24:44 -0700

    Update test code

--- postgresql/database/runtime4specialized.sql
+++ postgresql/database/runtime4specialized.sql
@@ -1,8 +1,25 @@
-/*\timing on
+/*
+\timing on
 select rules(842302);
 select rules(842517);
 select rules(84300903);
-\timing off*/
+select rules(84348301);
+select rules(84358402);
+select rules(843786);
+select rules(844359);
+select rules(84458202);
+select rules(844981);
+select rules(84501001);
+\timing off
+*/
 
-EXPLAIN ANALYZE
-select rules(842302);
+EXPLAIN ANALYZE select rules(842302);
+EXPLAIN ANALYZE select rules(842517);
+EXPLAIN ANALYZE select rules(84300903);
+EXPLAIN ANALYZE select rules(84348301);
+EXPLAIN ANALYZE select rules(84358402);
+EXPLAIN ANALYZE select rules(843786);
+EXPLAIN ANALYZE select rules(844359);
+EXPLAIN ANALYZE select rules(84458202);
+EXPLAIN ANALYZE select rules(844981);
+EXPLAIN ANALYZE select rules(84501001);
